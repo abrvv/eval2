38142d5ef381fff2bc1de253df1a847506e640b3
abrvv <wwcrmnnn@gmail.com>
2025-12-19 10:29:48 +0000

    ajout choix

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,22 @@ int main() {
   cout << "\n1 - Entrée dans le parking \n";
   cout << "2 - Nombre de places disponibles \n";
   cout << "3 - Sortie du parking \n";
-  cout << "4 - Quitter";
+  cout << "4 - Quitter \n";
+  cin >> choix;
+  switch(choix){
+    case 1:
+      cout << "Entrée";
+      break;
+    case 2:
+      cout << "Nb";
+      break;
+    case 3:
+      cout << "Sortie";
+      break;
+    case 4:
+      cout << "Quitter";
+      break;
+    default: cout << "Choisissez un chiffre entre 1 et 4";
+  }
   return 0;
 }
