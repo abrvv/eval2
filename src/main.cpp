0aadce0d7ed80673582c6ac8333e3ea517165c71
wwcrmnnn <31879980-wwcrmnnn@users.noreply.replit.com>
2025-12-19 11:05:58 +0000

    version finale

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() { 
   int nbPlaces = 10;
-  int choix;
+  char choix;
   cout << "************* Evaluation 2 ************* \n";
   cout << "\n1 - Entrée dans le parking \n";
   cout << "2 - Nombre de places disponibles \n";
@@ -13,23 +13,31 @@ int main() {
     do{
       cin >> choix;
       switch(choix){
-        case 1:
+        case '1':
+          if (nbPlaces > 0){
           nbPlaces--;
-          cout << "Entrée";
+          cout << "Entrée";     
+          }else{
+            cout << "Entrée impossible parking plein";
+          }         
           break;
-        case 2:
-          cout << "Il y a " + to_string(nbPlaces) +" places restantes";
+        case '2':
+          cout << "Il y a " + to_string(nbPlaces) +" place(s) restante(s)";
           break;
-        case 3:
-          nbPlaces++;
-          cout << "Sortie";
+        case '3':
+          if(nbPlaces < 10){
+            nbPlaces++;
+            cout << "Sortie";
+          }else {  
+            cout << "Parking vide";
+          }
           break;
-        case 4:
+        case '4':
           cout << "Au revoir \n";
           break;
         default: cout << "Choisissez un chiffre entre 1 et 4";
         break;  
       }
-    }while(choix != 4);
+    }while(choix != '4');
   return 0;
 }
