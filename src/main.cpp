f4e28198d62b8c3d68de8de5defbffa539a5e1ca
abrvv <wwcrmnnn@gmail.com>
2025-12-19 10:24:53 +0000

    2e commit

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,10 @@ using namespace std;
 int main() { 
   int nbPlaces = 10;
   int choix;
-  cout << "************* Evalutation 2 *************";
-
+  cout << "************* Evaluation 2 ************* \n";
+  cout << "\n1 - Entrée dans le parking \n";
+  cout << "2 - Nombre de places disponibles \n";
+  cout << "3 - Sortie du parking \n";
+  cout << "4 - Quitter";
   return 0;
 }
